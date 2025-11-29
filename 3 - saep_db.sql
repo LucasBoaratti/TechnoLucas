fb6ffd8bb65e8681ef9df1a246ebb38ba9bcf2c7
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-11-29 03:45:13 -0300

    Alteração dos comandos SQL.

--- 3 - saep_db.sql
+++ 3 - saep_db.sql
@@ -4,12 +4,19 @@ CREATE DATABASE saep_db;
 -- Usando o banco de dados
 USE saep_db;
 
+-- Populando a tabela de usuários
+INSERT INTO Usuarios (username, email, password, first_name, last_name, is_staff, is_active, is_superuser, last_login, date_joined)
+VALUES 
+	('Lucas', 'lucas@email.com', 'lucas123', '', '', 1, 1, 1, NOW(), NOW()),
+    ('Lindomar', 'lin@email.com', 'lin12345', '', '', 0, 1, 0, NOW(), NOW()),
+    ('Fernanda', 'fer@email.com', 'fer12345', '', '', 0, 1, 0, NOW(), NOW());
+
 -- Populando a tabela de produtos
 INSERT INTO Produtos (nome, tipo, quantidade_estoque, preco, descricao, responsavel_id)
 VALUES 
-    ("Notebook HP", "Notebook", 100, 99.99, "Um notebook excelente para você trabalhar a vontade em sua casa ou até na empresa!", 1),
-    ("SmartTV Sansung", "SmartTV", 50, 2.999, "SmartTV 4K UHD com acesso a aplicativos de streaming e integração com assistentes virtuais.", 1),
-    ("Smartphone Motorola Edge 40", "Smartphone", 200, 999.99, "Smartphone moderno com câmera de alta resolução, bateria de longa duração e tela OLED.", 1);
+    ('Notebook HP', 'Notebook', 100, 99.99, 'Um notebook excelente para você trabalhar a vontade em sua casa ou até na empresa!', 1),
+    ('SmartTV Sansung', 'SmartTV', 50, 2.999, 'SmartTV 4K UHD com acesso a aplicativos de streaming e integração com assistentes virtuais.', 1),
+    ('Smartphone Motorola Edge 40', 'Smartphone', 200, 999.99, 'Smartphone moderno com câmera de alta resolução, bateria de longa duração e tela OLED.', 1);
 
 -- Populando a tabela de históricos
 INSERT INTO Historicos (data_historico, responsavel_id, produto_id)
@@ -21,6 +28,6 @@ VALUES
 -- Populando a tabela de movimentações
 INSERT INTO Movimentacoes (quantidade_produtos, tipo_movimentacao, responsavel_id, produto_id)
 VALUES
-    (10, "Entrada", 1, 1),
-    (5, "Saída", 1, 2),
-    (20, "Entrada", 1, 3);
+    (10, 'Entrada', 1, 1),
+    (5, 'Saída', 1, 2),
+    (20, 'Entrada', 1, 3);
